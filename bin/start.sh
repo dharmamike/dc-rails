2117ee8963739ef065e52200cb6d811415ee0554
Mike Arnold <dharmamike@gmail.com>
2014-02-21 08:56:18 -0600

    detaching the server process

--- bin/start.sh
+++ bin/start.sh
@@ -4,4 +4,4 @@ bundle config build.pg --with-pg-config=/usr/bin/pg_config
 bundle install --gemfile=/home/app/webapp/firstapp/Gemfile
 bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rails s
+bundle exec rails s -d
